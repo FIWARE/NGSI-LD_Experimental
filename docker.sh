d11f666e939cebaeeb16c72088b1a6718c2cbc28
Dmitrii Demin <dmitrii.demin@fiware.org>
2018-05-18 18:52:39 +0200

    add: check pr

--- docker.sh
+++ docker.sh
@@ -3,6 +3,9 @@
 # check branch
 if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 0; fi
 
+# check PR
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then exit 0; fi
+
 # login
 echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
 
