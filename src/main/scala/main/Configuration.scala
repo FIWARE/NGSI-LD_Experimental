7a953d93b814fa1df4fd46b5f0f97ee55660bf71
Jose Manuel Cantera <josemanuel.cantera@fiware.org>
2018-09-11 10:21:35 +0200

    Making the NGSI-LD Wrapper now to pass 50 out of 62 tests

--- src/main/scala/main/Configuration.scala
+++ src/main/scala/main/Configuration.scala
@@ -6,8 +6,5 @@ trait Configuration {
   val NgsiEndpoint = "NGSI_Endpoint"
   val DefaultNgsiEndpoint = "http://localhost:1026"
   val DefaultPort = "1030"
-  val DefaultNgsiLdApiPath= "/ngsi-ld/v1"
-  val DefaultContextLink = """<http://uri.etsi.org/ngsi-ld/context.jsonld>;
-                             rel="http://www.w3.org/ns/json-ld#context";
-                             type="application/ld+json";"""
+  val DefaultNgsiLdApiPath = "/ngsi-ld/v1"
 }
